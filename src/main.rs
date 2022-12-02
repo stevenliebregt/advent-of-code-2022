da2e7f16bbe7fe64edf92783259192078c2783c0
Steven Liebregt <s.liebregt@outlook.com>
2022-12-02 07:52:36 +0100

    more horrible code for day 2

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ enum RoundResult {
 }
 
 fn main() {
+    part_1();
+    part_2();
+}
+
+fn part_1() {
     let file = File::open("resources/day_2.txt").expect("Could not open input");
     let mut reader = BufReader::new(file);
 
@@ -30,6 +35,75 @@ fn main() {
         let mut split = buffer.trim().split(' ');
         let opponent = split.next().unwrap();
         let you = split.next().unwrap();
+
+        let your_score = match you {
+            "X" =>  1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => unreachable!()
+        };
+
+        score += your_score;
+
+        let round_score = match round_result(opponent, you) {
+            RoundResult::Lose => 0,
+            RoundResult::Tie => 3,
+            RoundResult::Win => 6
+        };
+
+        score += round_score;
+    }
+
+    dbg!(&score);
+}
+
+
+fn part_2() {
+    let file = File::open("resources/day_2.txt").expect("Could not open input");
+    let mut reader = BufReader::new(file);
+
+    let mut buffer = String::new();
+
+    let mut score = 0;
+
+    loop {
+        buffer.clear();
+
+        let bytes_read = reader.read_line(&mut buffer).expect("Failed to read");
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        let mut split = buffer.trim().split(' ');
+        let opponent = split.next().unwrap();
+        let round_result_expected = split.next().unwrap();
+
+        let you = match round_result_expected {
+            // TODO: Isn't there some smart shifty way to do this?
+            // Need to lose
+            "X" =>  match opponent {
+                "A" => "Z",
+                "B" => "X",
+                "C" => "Y",
+                _ => unreachable!()
+            },
+            // Need to tie
+            "Y" => match opponent {
+                "A" => "X",
+                "B" => "Y",
+                "C" => "Z",
+                _ => unreachable!()
+            },
+            // Need to win (shift 1 back?)
+            "Z" => match opponent {
+                "A" => "Y",
+                "B" => "Z",
+                "C" => "X",
+                _ => unreachable!()
+            },
+            _ => unreachable!()
+        };
 
         let your_score = match you {
             "X" =>  1,
